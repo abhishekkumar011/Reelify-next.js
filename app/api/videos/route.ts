50cee780ab57a5ad9f61187c15fcb784fb1fd301
Abhishek Kumar <abhishekkumar0336165@gmail.com>
2025-04-22 17:57:00 +0530

    feat: add GET and POST endpoints for video upload and retrieval

---
+++ app/api/videos/route.ts
@@ -0,0 +1,68 @@
+import { dbConnect } from "@/lib/dbConnect";
+import { getServerSession } from "next-auth";
+import Video, { IVideo } from "@/models/Video.model";
+import { NextRequest, NextResponse } from "next/server";
+import { authOptions } from "../auth/[...nextauth]/options";
+
+export async function GET() {
+  try {
+    await dbConnect();
+    const videos = await Video.find({}).sort({ createdAt: -1 }).lean();
+
+    if (!videos || videos.length === 0) {
+      return NextResponse.json([], { status: 200 });
+    }
+
+    return NextResponse.json(videos);
+  } catch (error) {
+    console.error("Error fetching videos: ", error);
+    return NextResponse.json(
+      { error: "Failed to fetch videos: " },
+      { status: 500 }
+    );
+  }
+}
+
+export async function POST(request: NextRequest) {
+  try {
+    const session = await getServerSession(authOptions);
+
+    if (!session) {
+      return NextResponse.json({ error: "Unauthorized user" }, { status: 401 });
+    }
+
+    await dbConnect();
+    const body: IVideo = await request.json();
+
+    if (
+      !body.title ||
+      !body.description ||
+      !body.thumbnailUrl ||
+      !body.videoUrl
+    ) {
+      return NextResponse.json(
+        { error: "All fields are required" },
+        { status: 400 }
+      );
+    }
+
+    const videoData = {
+      ...body,
+      controls: body.controls ?? true,
+      transformation: {
+        height: 1920,
+        width: 1080,
+        quality: body.transformation?.quality ?? 100,
+      },
+    };
+
+    const newVideo = await Video.create(videoData);
+    return NextResponse.json({ message: "Video created succefully" }, newVideo);
+  } catch (error) {
+    console.error("Error creating video: ", error);
+    return NextResponse.json(
+      { error: "Failed to create video" },
+      { status: 500 }
+    );
+  }
+}
