1fd030c5e896e920cab9931168fa3dce7c8fec96
Abhishek Kumar <abhishekkumar0336165@gmail.com>
2025-04-24 17:35:12 +0530

    feat: create centralized ApiClient class for managing API calls

---
+++ lib/api-client.ts
@@ -0,0 +1,52 @@
+import { IVideo } from "@/models/Video.model";
+
+export type VideoFormData = Omit<IVideo, "_id">;
+
+type FetchOptions = {
+  method?: "GET" | "POST" | "PUT" | "DELETE";
+  body?: any;
+  headers?: Record<string, string>;
+};
+
+class ApiClient {
+  private async myfetch<T>(
+    endpoint: string,
+    options: FetchOptions = {}
+  ): Promise<T> {
+    const { method = "GET", body, headers = {} } = options;
+
+    const defaultHeaders = {
+      "Content-Type": "application/json",
+      ...headers,
+    };
+
+    const response = await fetch(`/api${endpoint}`, {
+      method,
+      headers: defaultHeaders,
+      body: body ? JSON.stringify(body) : undefined,
+    });
+
+    if (!response.ok) {
+      throw new Error(await response.text());
+    }
+
+    return response.json();
+  }
+
+  async getVideos() {
+    return this.myfetch<IVideo[]>("/videos");
+  }
+
+  async getAvideo(id: string) {
+    return this.myfetch(`/videos/${id}`);
+  }
+
+  async createVideo(videoData: VideoFormData) {
+    return this.myfetch("/videos", {
+      method: "POST",
+      body: videoData,
+    });
+  }
+}
+
+export const apiClient = new ApiClient();
